3b9dfa0bb8694e7c90debe708a9c4a2ce7ac6531
Anas Ansari <121580678+annasansari@users.noreply.github.com>
2023-03-09 20:35:23 +0500

    Add files via upload

---
+++ JS Assignment # 6/app.js
@@ -0,0 +1,126 @@
+// **Assignment # 06**
+
+// Question # 1
+
+// var firstName = prompt("Enter your first name")
+// var secondName = prompt("Enter your second name")
+// var fullName = ('Hello ' + firstName + " " + secondName)
+// alert(fullName)
+
+// Question # 2
+
+// var favMobile = prompt("Enter your favorite mobile")
+// for (var i = 0; i < favMobile.length; i++) {}
+// console.log('lenght of input is ' + i)
+
+// Question # 3
+
+// var str = 'pakistani'
+// console.log('Index is ' + str.indexOf('n'))
+
+// Question # 4
+
+// var str = ' Hello world'
+// console.log('Last Index of l is ' + str.lastIndexOf('l'))
+
+// Question # 5
+
+// var str = 'Pakistani'
+// console.log(str.indexOf[1])
+// confusion
+
+// Question # 6
+
+// var firstName = 'Anas'
+// var secondName = 'Ansari'
+// console.log(firstName.concat(" ", secondName))
+
+// Question # 7
+
+// var cityName = 'Islamabad'
+// console.log(cityName)
+// console.log("After replacement " + cityName.replace('Islam', 'Hyder'))
+
+// Question # 8
+
+// var str = 'Ali and Sami are best friends. They play cricket and football together'
+// console.log(str.replaceAll('and', '&'))
+
+// Question # 9
+
+// var num = 420
+// console.log(num.toString())
+
+// Question # 10
+
+// var str = 'peanuts'
+// console.log(str.toUpperCase())
+
+// Question # 11
+
+// var str = 'javascript'
+// var firstLetter = str.split("")
+// var capLetter = firstLetter[0].toUpperCase()
+// console.log(capLetter + str.slice(1))
+
+
+// Question # 12
+
+// var num = 36.34
+// var str = num.toString()
+// console.log(str.replace(".", ""))
+
+// Question # 13
+
+// var userName = 'Anas ! Ansari'
+// for (var i = 0; i < userName.length; i++) {
+//     // console.log(userName[i])
+//     if (userName.charCodeAt(33) != userName[i]) {
+//         alert("Invalid Input")
+//         break
+//     } else {
+//         alert("Correct Input")
+//     }
+// }
+//Confusion
+
+
+// Question # 14
+
+// var items = ['cake', 'apple', 'pie', 'cookie', 'chips', 'patties']
+// var userInput = prompt("Enter Items")
+// for (var i = 0; i < items.length; i++) {
+//     if (userInput == items.length) {
+//         alert("available")
+//         break
+//     } else {
+//         alert("not availabe")
+//         break
+//     }
+// }
+// console.log(items[i])
+
+//  Question # 15
+
+// Required
+
+
+// Question # 16
+
+// var str = "University of Karachi"
+// var strSplite = str.split("")
+// for (var i = 0; i < strSplite.length; i++) {
+//     document.write(strSplite[i] + "<br>")
+// }
+
+// Question # 17
+
+// var str = prompt("Enter your Input")
+// console.log("Last character of input is " + str.charAt(str.length - 1))
+
+// Question # 18
+
+
+
+
+//
